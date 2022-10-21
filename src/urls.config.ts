f364d0917c15888a5915f20993b983f7772767ad
Anton Mushnin <anton.mushnin@gmail.com>
2022-10-21 17:28:28 +0300

    build assets

--- src/urls.config.ts
+++ src/urls.config.ts
@@ -2,7 +2,7 @@
 
 // export const assetsPath = import.meta.env.VITE_ASSETS_URL_PREFIX// 123
 
-export const assetsPath = (import.meta.env.PROD ? 'memo-minecraft' : 'src/assets') + '/images/';
+export const assetsPath = (import.meta.env.PROD ? '/memo-minecraft' : 'src/assets') + '/images/';
 
 
 // export const assetsPath = '/images/';
